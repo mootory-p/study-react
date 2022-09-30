c82eb4ea65e28229241910ed9e1ff30f6f5f03a4
mootory-p <mootory@naver.com>
2022-09-30 12:35:34 +0900

    offset

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -13,11 +13,11 @@ const Nav = () => {
       {/* <a href="#" className={ activeNav === '#' ? 'active' : '' } onClick={ ()=> setactiveNav('#') }><BiHomeAlt /></a>
       <a href="#about" className={ activeNav === '#about' ? 'active' : '' } onClick={ ()=> setactiveNav('#about') }><FiMail /></a>
       <a href="#contact" className={ activeNav === '#contact' ? 'active' : '' } onClick={ ()=> setactiveNav('#contact') }><FiMail /></a> */}
-      <Link to="pagetop" smooth={true} offset={-50} duration={500} className={ activeNav === '#' ? 'active' : '' } onClick={ ()=> setactiveNav('#') }><BiHomeAlt /></Link>
-      <Link to="about"  smooth={true} offset={-50} duration={500} className={ activeNav === '#about' ? 'active' : '' } onClick={ ()=> setactiveNav('#about') }><FiMail /></Link>
-      <Link to="contact" smooth={true} offset={-50} duration={500} className={ activeNav === '#contact' ? 'active' : '' } onClick={ ()=> setactiveNav('#contact') }><FiMail /></Link>
-      <Link to="study" smooth={true} offset={-50} duration={500} className={ activeNav === '#study' ? 'active' : '' } onClick={ ()=> setactiveNav('#study') }><FiMail /></Link>
-      <Link to="footer" smooth={true} offset={10} duration={500} className={ activeNav === '#footer' ? 'active' : '' } onClick={ ()=> setactiveNav('#footer') }><FiMail /></Link>
+      <Link to="pagetop" smooth={true} offset={-100} duration={500} className={ activeNav === '#' ? 'active' : '' } onClick={ ()=> setactiveNav('#') }><BiHomeAlt /></Link>
+      <Link to="about"  smooth={true} offset={-100} duration={500} className={ activeNav === '#about' ? 'active' : '' } onClick={ ()=> setactiveNav('#about') }><FiMail /></Link>
+      <Link to="contact" smooth={true} offset={-100} duration={500} className={ activeNav === '#contact' ? 'active' : '' } onClick={ ()=> setactiveNav('#contact') }><FiMail /></Link>
+      <Link to="study" smooth={true} offset={-100} duration={500} className={ activeNav === '#study' ? 'active' : '' } onClick={ ()=> setactiveNav('#study') }><FiMail /></Link>
+      <Link to="footer" smooth={true} offset={-50} duration={500} className={ activeNav === '#footer' ? 'active' : '' } onClick={ ()=> setactiveNav('#footer') }><FiMail /></Link>
     </nav>
     
   )
